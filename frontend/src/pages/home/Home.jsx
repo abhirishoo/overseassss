d36028fb38cd817106f15f36952d2d31db72ef90
Abhijeet Rishoo <abhi.rishoo2003@gmail.com>
2024-04-05 12:01:38 +0530

    font

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,14 +4,8 @@ import Sidebar from "../../components/sidebar/Sidebar";
 const Home = () => {
 	return (
 		<div className='flex rounded-lg w-full overflow-hidden bg-zinc-900'>
-			<div className="w-1/4">
-
 			<Sidebar />
-			</div>
-			<div className="w-3/4">
-
 			<MessageContainer />
-			</div>
 		</div>
 	);
 };
